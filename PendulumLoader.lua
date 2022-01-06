e12c8422fe9a1bdfbb49d71aa5af25d7ac026b5a
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2022-01-07 00:58:43 +0100

    Release?

---
+++ PendulumLoader.lua
@@ -0,0 +1,279 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Error-Cezar/Pendulum-Hub/main/PendulumUI.lua"))()
+
+local Pendulum = Library.CreateGui("Pendulum Hub")
+
+AddButton("Neptunian V", "An original. If you want the hat join the discord.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Neptunia.lua"))()
+end)
+
+AddButton("Sonic", "All other versions don't fling except this one.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/FESonic.lua"))()
+end)
+
+AddButton("Neko", "This one is kinda self explanatory lol.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/neko.lua"))()
+end)
+
+AddButton("Joy", "Its got some cute stufff", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Joy.lua"))()
+end)
+
+AddButton("Elio Blasio", "Literally anyone with a gun. If you want hats join the discord.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Elio%20Balsio.lua"))()
+end)
+
+AddButton("Ender", "Smashy boi", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/FinalEnderVersionDeObfuscated.lua"))()
+end)
+
+AddButton("KillBot V2", "The script kinda sucks lol", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/KillbotV2.lua"))()
+end)
+
+AddButton("Star Platinum Over Heaven", "A really fun script for people who have played a jojo game before.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/FEStandConverted.lua"))()
+end)
+
+AddButton("Chill", "You can get a lot of peoples attention with this.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/FEChill.lua"))()
+end)
+
+AddButton("Lutris v2", "Definetely an OP script. Join discord for hat", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Lustris%20V2.lua"))()
+end)
+
+AddButton("Memeus v2.5", "Dead memes from 2019 (some arent dead)", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Memeus.lua"))()
+end)
+
+AddButton("Meme animation", "This one is just some emotes and dances.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/memeanimations.lua"))()
+end)
+
+AddButton("Krystal dance", "Some nice dances. My favorite is U.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Krystal%20Dance.lua"))()
+end)
+
+AddButton("Spinning Blade", "Definetely has the best idle animation", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/FEPen.lua"))()
+end)
+
+AddButton("Xester", "The strongest script out of them all.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/FEXESTERFINALLY!!!!!!!.lua"))()
+end)
+
+AddButton("Caducus", "Edgy demon guy.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/cadacus.lua"))()
+end)
+
+AddButton("Minigun", "Have fun spraying skids.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/FEminigun.lua"))()
+end)
+
+AddButton("Lightning Sword", "Similar to Dual Ultima. its basiclly an edit.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/LightningSword.lua"))()
+end)
+
+AddButton("Corrupted Overseer", "My favorite script.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Corrupted%20Overseer.lua"))()
+end)
+
+AddButton("Darth Vadar", "For the star wars fans.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Darth%20Vadar.lua"))()
+end)
+
+AddButton("Sans", "left click to fling. time it with attacks to fling.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/FESans.lua"))()
+end)
+
+AddButton("Zen", "literally zenyata", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/FEZen.lua"))()
+end)
+
+AddButton("Gale Fighter", "Another classic!", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Gale%20Fighter.lua"))()
+end)
+
+AddButton("Glove & Sword", "The script isn't broken press f to equip.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Glove%20and%20sword.lua"))()
+end)
+
+AddButton("Reaper", "Reaper from Overwatch", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Reaper.lua"))()
+end)
+
+AddButton("Zenith Rifle", "Added because of high request.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Zenith%20rifle.lua"))()
+end)
+
+AddButton("Abyss Eye", "This one is very intimidating.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/AbyssEye.lua"))()
+end)
+
+AddButton("Bizzaro", "TPose thing", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Bizzaro.lua"))()
+end)
+
+AddButton("Sword Guy", "I don't even know the name LOL.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Swordthing.lua"))()
+end)
+
+AddButton("Sharpshooter", "Laser finger thing", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Sharpshooter.lua"))()
+end)
+
+AddButton("Dual Pink Guns", "Reminds me of the matrix for some reason.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Dual%20pink%20guns.lua"))()
+end)
+
+AddButton("Aureate", "This script can kill your frames in big games.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Aureate.lua"))()
+end)
+
+AddButton("Neptunian Gun & Sword", "Join discord for hats.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Script_4.lua"))()
+end)
+
+AddButton("Doomspire Brickbattler", "For people who say that old roblox is better.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/DoomspireBrickbattler.lua"))()
+end)
+
+AddButton("Star Glitcher", "A free version that doesn't need any hats.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Free%20Star%20Glitcher.lua"))()
+end)
+
+AddButton("Nightmare Sans", "From Dream!tale. Very strong.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Nightmare%20Sans.lua"))()
+end)
+
+AddButton("Abyss Sword", "The first script I converted that uses a gear.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Abyssal%20sword.lua"))()
+end)
+
+AddButton("Dark Magic", "If you've played Black Magic you'll love this script.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Magic.lua"))()
+end)
+
+AddButton("Torando Gauntlet", "Gauntlet that makes tornados.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Tornado%20Gauntlet.lua"))()
+end)
+
+AddButton("Hidden blades - From AC", "This ones cool.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Hidden%20Blades.lua"))()
+end)
+
+AddButton("Despira", "This one is a balanced range and close ranged script.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Despira.lua"))()
+end)
+
+AddButton("Golden Fireball God", "This somehow reminds me of Asgore.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Golden%20Fireball%20God.lua"))()
+end)
+
+AddButton("Simple Sword", "There isn't much to it.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Katanathing.lua"))()
+end)
+
+AddButton("Brutal Anti Furry", "Damn you must hate furrys.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Anti%20Furry%20lol.lua"))()
+end)
+
+AddButton("Omni God", "A stupidly powerful script if you know how to use fling properly.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Omni%20God.lua"))()
+end)
+
+AddButton("Baldi", "https://www.youtube.com/watch?v=SciE-AbMLt0", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Baldi%20FE.lua"))()
+end)
+
+AddButton("Shedletsky Rage", "Not to much about it.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Shedletsky%20Rage.lua"))()
+end)
+
+AddButton("Nebula Star Glitcher", "Yes it has the big black lol.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Nebula%20Star%20Glitcher.lua"))()
+end)
+
+AddButton("Lustris PURPLE", "Purple version of Lustris and different powers", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Lustris%20PURPLE.lua"))()
+end)
+
+AddButton("Touhou Magic", "Rain bullet hell on em bitch.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Touhou%20Script.lua"))()
+end)
+
+AddButton("Excalibur", "Its just a cool sword thing", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Excalibur.lua"))()
+end)
+
+AddButton("John Doe", "Yeah I added him back with better fling", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/John%20Doe.lua"))()
+end)
+
+AddButton("What another one", "Yes thats the scripts name Mr. Skiddy Titty", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/What%20another%20one.lua"))()
+end)
+
+AddButton("Ether", "Do people even read these?", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Evolution-Hub/main/Ether.lua"))()
+end)
+
+AddButton("Uncle Parlo The Redneck", "Parlo is not a pedophile I swear!", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Uncle%20Parlo%20The%20Stupid%20Fucking%20Redneck.lua"))()
+end)
+
+AddButton("Eyo Zen", "Guy with eyeball that shoots lasers.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Eyo%20Zen.lua"))()
+end)
+
+AddButton("Cop", "Your average cop.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Cop.lua"))()
+end)
+
+AddButton("Verlex", "Very unique script that i've seen.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Verlex.lua"))()
+end)
+
+AddButton("Lost Hope", "Dragon Scythe thing and its cool", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Lost%20Hope.lua"))()
+end)
+
+AddButton("The Assasian", "A shit ton of moves that are epic", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/The%20Assasian.lua"))()
+end)
+
+AddButton("Dragonian Pyramus", "Literally the fucking ender dragon.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Dragonian%20Pyramus.lua"))()
+end)
+
+AddButton("Grappler", "You can go to a city game and use this.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Grappler.lua"))()
+end)
+
+AddButton("Groundbreaker Gauntlets", "Gauntlets of death", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Groundbreaker%20Gauntlets.lua"))()
+end)
+
+AddButton("Sexy Staff Girl", "Gauntlets of death", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Sexy%20Staff%20Girl.lua"))()
+end)
+
+AddButton("Jojo Super Gauntlets", "Why are there so many gauntlet scripts?", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Jojo%20Super%20Gauntlets.lua"))()
+end)
+
+AddButton("The Pacifist", "No attacks. Just a very chill animation script.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/The%20Pacifist.lua"))()
+end)
+
+AddButton("Sakura Blade", "Barely any jitter in these animations.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Sakura%20Blade.lua"))()
+end)
+
+AddButton("Lightning Cannon", "Shoots Lightning. Skids 101.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Lightning%20Cannon.lua"))()
+end)
+
+AddButton("M41451", "Rainbow AK47 needed join the discord for hat.", function()
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Lightning%20Cannon.lua"))()
+end)
